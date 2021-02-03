aa9db02c68e7a65f3e1dd3fb1c9c3d9e7826b2b5
Jason Barrie Morley <jbmorley@mac.com>
2021-02-02 17:44:56 -0800

    Add a toolbar with support for toggling favourites.

--- Photo Export/Photo Export/ContentView.swift
+++ Photo Export/Photo Export/ContentView.swift
@@ -17,14 +17,17 @@ struct CollectionView: View {
 
     @ObservedObject var manager: Manager // TODO: This probably doesn't require a manager.
     @ObservedObject var collection: Collection
+    @State var showOnlyFavorites: Bool = false
 
     static let spacing: CGFloat = 8
     let columns = [GridItem(.adaptive(minimum: 200, maximum: 200), spacing: spacing)]
 
+    var photos: [Photo] { collection.photos.filter { !showOnlyFavorites || $0.asset.isFavorite } }
+
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns, spacing: Self.spacing) {
-                ForEach(collection.photos) { photo in
+                ForEach(photos) { photo in
                     Thumbnail(manager: manager, photo: photo)
                         .contextMenu(ContextMenu(menuItems: {
                             Button {
@@ -46,6 +49,30 @@ struct CollectionView: View {
             }
             .padding()
         }
+        .toolbar {
+            ToolbarItem {
+                Button {
+
+                } label: {
+                    Image(systemName: "square.and.arrow.down")
+                        .foregroundColor(.primary)
+                }
+            }
+            ToolbarItem {
+                Button {
+                    showOnlyFavorites = !showOnlyFavorites
+                } label: {
+                    if showOnlyFavorites {
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(.primary)
+                    } else {
+                        Image(systemName: "heart")
+                            .foregroundColor(.primary)
+                    }
+                }
+            }
+        }
+        .navigationTitle(collection.localizedTitle)
     }
 }
 
