ab89af423abeee8891782e658b10fc0ca49002d2
Jason Barrie Morley <jbmorley@mac.com>
2021-02-04 11:30:29 -0800

    Separate file for Collection.

---
+++ Photo Export/Photo Export/Collection.swift
@@ -0,0 +1,38 @@
+//
+//  Collection.swift
+//  Photo Export
+//
+//  Created by Jason Barrie Morley on 04/02/2021.
+//
+
+import Foundation
+import Photos
+
+class Collection: ObservableObject, Identifiable {
+
+    var id: String { collection.id }
+
+    @Published var photos: [Photo] = []
+
+    weak var manager: Manager?
+    var collection: PHAssetCollection
+
+    var localizedTitle: String {
+        collection.localizedTitle ?? "Untitled"
+    }
+
+    init(manager: Manager, collection: PHAssetCollection) {
+        self.manager = manager
+        self.collection = collection
+
+        let options = PHFetchOptions()
+        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+
+        let result = PHAsset.fetchAssets(in: collection, options: options)
+        result.enumerateObjects { asset, index, stop in
+            dispatchPrecondition(condition: .onQueue(.main))
+            self.photos.append(Photo(manager: manager, asset: asset))
+        }
+    }
+
+}
--- Photo Export/Photo Export/Manager.swift
+++ Photo Export/Photo Export/Manager.swift
@@ -24,36 +24,6 @@ enum ManagerError: Error {
     case unknown
 }
 
-class Collection: ObservableObject, Identifiable {
-
-    var id: String { collection.id }
-
-    @Published var photos: [Photo] = []
-
-    // TODO: This should probably be weak??
-    var manager: Manager
-    var collection: PHAssetCollection
-
-    var localizedTitle: String {
-        collection.localizedTitle ?? "Untitled"
-    }
-
-    init(manager: Manager, collection: PHAssetCollection) {
-        self.manager = manager
-        self.collection = collection
-
-        let options = PHFetchOptions()
-        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-
-        let result = PHAsset.fetchAssets(in: collection, options: options)
-        result.enumerateObjects { asset, index, stop in
-            dispatchPrecondition(condition: .onQueue(.main))
-            self.photos.append(Photo(manager: manager, asset: asset))
-        }
-    }
-
-}
-
 class ExportTask: Operation {
 
     override var isAsynchronous: Bool { false }
